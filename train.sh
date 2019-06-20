e9873de67340dd6d56faa2a44b3eddf093374cf4
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2019-06-20 16:30:16 +0900

    epoch 400

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ GPU=${GPU:--1}
 
 python -u train.py \
  -b 128 --log-interval 100 -g ${GPU} --test data/prep-title.txt \
-  data/prep-title.txt data/vocab.txt --valid-interval 50
+  data/prep-title.txt data/vocab.txt --valid-interval 50 --epoch 400
